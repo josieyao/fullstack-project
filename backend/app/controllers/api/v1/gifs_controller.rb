d76dd8408b8624a33638d87fc5f9750df26565c4
josieyao <yao.josephine@yahoo.com>
2021-03-22 23:05:03 -0700

    added search gifs logic with giphy api

--- backend/app/controllers/api/v1/gifs_controller.rb
+++ backend/app/controllers/api/v1/gifs_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::GifsController < ApplicationController
+    def search_gifs
+        @gifs = giphy.search(params[:search])
+        if @gifs.nil?
+            render json: { error: "Unable to process request" }, status: 400
+        else
+            render json: @gifs, status: 200
+        end
+    end
+
+    private
+    def giphy
+        @giphy = Giphy.new(Rails.application.credentials.giphy[:secret_access_key])
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :favorites
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+    namespace :v1 do
+      get 'gifs/search', to: 'gifs#search_gifs'
+      # get 'favorites', to: 'favorites#index'
+      # post 'favorites', to: 'favorites#create'
+      resources :favorites
+    end
   end
 end
