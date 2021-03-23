6328d78858d0673a52ba677a08a73b6e36f1c50f
josieyao <yao.josephine@yahoo.com>
2021-03-23 16:57:23 -0700

    added axios and material ui packages to frontend, removed App template

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'gifs/search', to: 'gifs#search_gifs'
-      # get 'favorites', to: 'favorites#index'
-      # post 'favorites', to: 'favorites#create'
       resources :favorites
     end
   end
